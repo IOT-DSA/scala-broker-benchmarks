4fbfdbe1a68bf9fd883050f8940bd10a670eab88
Vlad Orzhekhovskiy <vlad@uralian.com>
2017-08-28 16:56:38 -0400

    Make LIST path for BrokerExploreTest configurable

--- src/main/scala/org/dsa/iot/benchmark/Settings.scala
+++ src/main/scala/org/dsa/iot/benchmark/Settings.scala
@@ -4,5 +4,8 @@ package org.dsa.iot.benchmark
  * Benchmark settings.
  */
 object Settings {
+  
   val BrokerUrl = scala.util.Properties.envOrElse("dsa.brokerUrl", "http://localhost:8080/conn")
+  
+  val ExplorePath = scala.util.Properties.envOrElse("dsa.explore.path", "/defs/profile")
 }
