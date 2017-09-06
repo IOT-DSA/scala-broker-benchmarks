bf80fb33c60ef7848acbef5ed62729ffc583cdd3
Vlad Orzhekhovskiy <vlad@uralian.com>
2017-09-06 13:47:29 -0400

    Add BrokerConnectionTest to the suite

--- src/main/scala/org/dsa/iot/benchmark/RunTestSuite.scala
+++ src/main/scala/org/dsa/iot/benchmark/RunTestSuite.scala
@@ -3,9 +3,17 @@ package org.dsa.iot.benchmark
 import org.slf4j.LoggerFactory
 
 object RunTestSuite extends App {
+  import Settings.Suite._
+
   val log = LoggerFactory.getLogger(getClass)
-  
+  val noargs = Array.empty[String]
+
   log.info("Starting DSA Broker Test Suite")
-  
-  SampleResponder.main(Array.empty[String])
+
+  if (runConnectionTest) {
+    BrokerConnectionTest.main(noargs)
+    Thread.sleep(2000)
+  }
+
+  SampleResponder.main(noargs)
 }
