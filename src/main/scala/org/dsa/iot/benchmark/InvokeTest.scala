2f1ac42bdec37c08143b8c7281c87c5bb36f9997
Vlad Orzhekhovskiy <vlad@uralian.com>
2017-09-06 14:43:33 -0400

    Implement Invoke test

---
+++ src/main/scala/org/dsa/iot/benchmark/InvokeTest.scala
@@ -0,0 +1,55 @@
+package org.dsa.iot.benchmark
+
+import scala.util.Random
+
+import org.dsa.iot.dslink.link.Requester
+import org.dsa.iot.scala.DSAHelper
+import org.slf4j.LoggerFactory
+
+/**
+ * Usage: InvokeTest [linkId]
+ */
+object InvokeTest extends App {
+  import Settings._
+  import org.dsa.iot.scala.LinkMode._
+
+  val log = LoggerFactory.getLogger(getClass)
+
+  log.info("Starting Invoker Test")
+
+  val id = args.headOption getOrElse Random.alphanumeric.take(5).mkString.capitalize
+  val connector = createConnector(id, "/requester.json")
+  val connection = connector.start(REQUESTER)
+  implicit val requester = connection.requester
+
+  sys.addShutdownHook {
+    connector.stop
+  }
+
+  val thread = new Thread {
+    override def run = try {
+      while (true) {
+        benchmarkInvoke(Invoke.BatchSize)(requester)
+        pause(Invoke.Timeout)
+      }
+    } catch {
+      case e: InterruptedException => log.info("Thread interruped")
+    }
+  }
+  thread.start
+
+  /**
+   * Executes a batch of INVOKE commands and returns the average execution time per command.
+   */
+  private def benchmarkInvoke(iterations: Int)(implicit requester: Requester) = {
+    val start = System.currentTimeMillis
+    (1 to iterations) foreach { _ =>
+      val idx = Random.nextInt(Responder.NodeCount) + 1
+      val path = s"/downstream/${Responder.Id}/counter$idx/incCounter"
+      DSAHelper invoke path
+    }
+    val end = System.currentTimeMillis
+
+    (end - start) / iterations.toDouble
+  }
+}
--- src/main/scala/org/dsa/iot/benchmark/Settings.scala
+++ src/main/scala/org/dsa/iot/benchmark/Settings.scala
@@ -1,5 +1,7 @@
 package org.dsa.iot.benchmark
 
+import scala.concurrent.duration._
+
 /**
  * Benchmark settings.
  */
@@ -14,5 +16,10 @@ object Settings {
     val Id = props.envOrElse("responder.id", "SampleResponder")
     val NodeCount = props.envOrElse("responder.nodeCount", "100").toInt
     val AttributeCount = props.envOrElse("responder.attributeCount", "100").toInt
+  }
+  
+  object Invoke {
+    val BatchSize = props.envOrElse("invoke.batchSize", "10").toInt
+    val Timeout = props.envOrElse("invoke.timeout", "1000").toInt milliseconds
   }
 }
