76aabc7dd6e70fb9796992391f96ee572ec881cc
Vlad Orzhekhovskiy <vlad@uralian.com>
2018-02-19 16:15:11 -0500

    Implement BenchmarkResponder

---
+++ src/main/scala/org/dsa/iot/actors/entities.scala
@@ -0,0 +1,16 @@
+package org.dsa.iot.actors
+
+/**
+ * Similar to java AtomicInteger, but not thread safe,
+ * optimized for single threaded execution by an actor.
+ */
+class IntCounter(init: Int = 0) {
+  private var value = init
+
+  @inline def get = value
+  @inline def inc = {
+    val result = value
+    value += 1
+    result
+  }
+}
