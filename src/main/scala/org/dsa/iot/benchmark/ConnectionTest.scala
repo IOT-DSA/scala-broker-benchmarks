cd3a07f0607c7a5a9a9cc5ff6cd9dc9b07596d4e
Vlad Orzhekhovskiy <vlad@uralian.com>
2018-08-23 16:23:43 -0400

    Fix connection test NPE

--- src/main/scala/org/dsa/iot/benchmark/ConnectionTest.scala
+++ src/main/scala/org/dsa/iot/benchmark/ConnectionTest.scala
@@ -2,9 +2,9 @@ package org.dsa.iot.benchmark
 
 import akka.actor.{Actor, ActorRef, ActorSystem}
 import akka.stream.ActorMaterializer
-import org.dsa.iot.actors.{BenchmarkResponder, BenchmarkResponderConfig, LinkType}
+import org.dsa.iot.actors.{BenchmarkResponder, BenchmarkResponderConfig, LinkType, StatsCollector}
 import org.dsa.iot.handshake.LocalKeys
-import org.dsa.iot.util.EnvUtils
+import org.dsa.iot.util.InfluxClient
 import org.dsa.iot.ws.WebSocketConnector
 import org.slf4j.LoggerFactory
 
@@ -24,7 +24,12 @@ object ConnectionTest extends App {
 
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
+
   implicit val ec = system.dispatcher
+
+  val influx = InfluxClient.getInstance
+
+  val collector = system.actorOf(StatsCollector.props(influx, false))
 
   val connector = new WebSocketConnector(LocalKeys.generate)
   val dslinkName = "benchmark-test"
@@ -34,7 +39,7 @@ object ConnectionTest extends App {
     val collateAutoIncUpdates: Boolean = false
   }
 
-  val propsFunc = (out: ActorRef) => BenchmarkResponder.props(dslinkName, out, Actor.noSender, cfg)
+  val propsFunc = (out: ActorRef) => BenchmarkResponder.props(dslinkName, out, collector, cfg)
   val connection = connector.connect(dslinkName, brokerUrl, LinkType.Responder, propsFunc)
 
   connection foreach { conn =>
@@ -42,6 +47,7 @@ object ConnectionTest extends App {
     conn.terminate()
     Thread.sleep(1000)
     log.info("Connection to {} shut down", brokerUrl)
+    influx.close()
     sys.exit
   }
 }
